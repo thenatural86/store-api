9c23f8b62b930cc88bf262402efca1057fae1b1f
thenatural86 <philip.smalls86@gmail.com>
2024-03-17 14:18:52 -0300

    select

--- controllers/products.js
+++ controllers/products.js
@@ -1,12 +1,12 @@
 const Product = require('../models/product')
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find().sort('name price')
+  const products = await Product.find().sort('name price').select('name price')
   res.status(200).json({ products, nbHits: products.length })
 }
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name, sort } = req.query
+  const { featured, company, name, sort, fields } = req.query
   const queryObject = {}
 
   if (featured) {
@@ -20,12 +20,20 @@ const getAllProducts = async (req, res) => {
   }
 
   let result = Product.find(queryObject)
+
+  // sort
   if (sort) {
     const sortList = sort.split(',').join('')
     console.log(result)
     result = result.sort(sortList)
   } else {
     result = result.sort('createdAt')
+  }
+
+  // select
+  if (fields) {
+    const fieldsList = fields.split(',').join('')
+    result = result.select(fieldsList)
   }
 
   const products = await result
