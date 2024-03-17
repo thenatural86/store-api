b6f2066de147ad9f387263b438b0a356d2117225
thenatural86 <philip.smalls86@gmail.com>
2024-03-17 17:17:55 -0300

    select

--- controllers/products.js
+++ controllers/products.js
@@ -1,7 +1,11 @@
 const Product = require('../models/product')
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find().sort('name price').select('name price')
+  const products = await Product.find()
+    .sort('name price')
+    .select('name price')
+    .limit(10)
+    .skip(1)
   res.status(200).json({ products, nbHits: products.length })
 }
 
