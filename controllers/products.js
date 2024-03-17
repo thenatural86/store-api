1da2425dce07f80fac1bfdeda5d54b7ff56772c9
thenatural86 <philip.smalls86@gmail.com>
2024-03-17 17:32:49 -0300

    pagination

--- controllers/products.js
+++ controllers/products.js
@@ -28,7 +28,6 @@ const getAllProducts = async (req, res) => {
   // sort
   if (sort) {
     const sortList = sort.split(',').join('')
-    console.log(result)
     result = result.sort(sortList)
   } else {
     result = result.sort('createdAt')
@@ -39,6 +38,12 @@ const getAllProducts = async (req, res) => {
     const fieldsList = fields.split(',').join('')
     result = result.select(fieldsList)
   }
+
+  const page = Number(req.query.page) || 1
+  const limit = Number(req.query.limit) || 10
+  const skip = (page - 1) * limit
+
+  result = result.skip(skip).limit(limit)
 
   const products = await result
   res.status(200).json({ products, nbHits: products.length })
