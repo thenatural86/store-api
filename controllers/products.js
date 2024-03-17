8a266aa029b83f4e8191251cd3fe4cacf52da2f2
thenatural86 <philip.smalls86@gmail.com>
2024-03-17 10:46:56 -0300

    more sorting

--- controllers/products.js
+++ controllers/products.js
@@ -6,11 +6,11 @@ const getAllProductsStatic = async (req, res) => {
 }
 
 const getAllProducts = async (req, res) => {
-  const { featured, company, name } = req.query
+  const { featured, company, name, sort } = req.query
   const queryObject = {}
 
   if (featured) {
-    queryObject.featured = featured === true ? true : false
+    queryObject.featured = featured === 'true' ? true : false
   }
   if (company) {
     queryObject.company = company
@@ -19,7 +19,16 @@ const getAllProducts = async (req, res) => {
     queryObject.name = { $regex: name, $options: 'i' }
   }
 
-  const products = await Product.find(queryObject)
+  let result = Product.find(queryObject)
+  if (sort) {
+    const sortList = sort.split(',').join('')
+    console.log(result)
+    result = result.sort(sortList)
+  } else {
+    result = result.sort('createdAt')
+  }
+
+  const products = await result
   res.status(200).json({ products, nbHits: products.length })
 }
 
